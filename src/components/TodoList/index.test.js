b49513a97c4bd4a1c71c7b826d78ffea951474d3
KIM BEOMJOON <p1n9@KIMs-MacBook-Air.local>
2023-12-27 22:28:02 +0900

    test(todo-list): add test that filter list

--- src/components/TodoList/index.test.js
+++ src/components/TodoList/index.test.js
@@ -1,13 +1,18 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import { TODO_STATUS } from "constant";
 import { TodoContext } from "context/todoContext";
 import TodoList from ".";
-import { TODO_STATUS } from "constant";
 
 const _mock = [
     {
         id: "1",
         text: "Todo 1",
         status: TODO_STATUS.active,
+    },
+    {
+        id: "2",
+        text: "Todo 2",
+        status: TODO_STATUS.completed,
     },
 ];
 
@@ -25,5 +30,27 @@ describe("TodoList", () => {
         );
         const todosElement = screen.queryAllByRole("listitem");
         expect(todosElement).toHaveLength(_mock.length);
+    });
+
+    test.each(Object.keys(TODO_STATUS))("filter list", (filter) => {
+        render(
+            <TodoContext.Provider
+                value={{
+                    state: _mock,
+                    dispatch: jest.fn(),
+                }}
+            >
+                <TodoList />
+            </TodoContext.Provider>,
+        );
+
+        const completedElement = screen.queryByText(filter);
+        fireEvent.click(completedElement);
+        const todosElements = screen.queryAllByRole("listitem");
+        const filterTodos = _mock.filter(
+            (item) => item.status === TODO_STATUS[filter],
+        );
+
+        expect(todosElements).toHaveLength(filterTodos.length);
     });
 });

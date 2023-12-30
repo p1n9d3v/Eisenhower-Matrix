fa4bf0320ad59c43dab480523e06ff1e336c35f6
KIM BEOMJOON <p1n9@KIMs-MacBook-Air.local>
2023-12-30 22:21:18 +0900

    style(AddTodoForm): styling

--- src/components/AddTodoForm/index.jsx
+++ src/components/AddTodoForm/index.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import styles from "./index.module.css";
+import { MdPlaylistAdd } from "react-icons/md";
 
 function AddTodoForm({ onAddTodo }) {
     const [text, setText] = useState("");
@@ -18,13 +20,23 @@ function AddTodoForm({ onAddTodo }) {
 
     return (
         <form aria-label="add todo" onSubmit={onSubmit}>
-            <input
-                type="text"
-                value={text}
-                onChange={onChange}
-                placeholder="Add new todo here..."
-            />
-            <button type="submit">Add</button>
+            <div className={styles.inputContainer}>
+                <input
+                    className={styles.input}
+                    type="text"
+                    value={text}
+                    onChange={onChange}
+                    placeholder="Add new todo here..."
+                />
+                <button type="submit" className={styles.button}>
+                    <MdPlaylistAdd
+                        style={{
+                            width: "24px",
+                            height: "24px",
+                        }}
+                    />
+                </button>
+            </div>
         </form>
     );
 }
